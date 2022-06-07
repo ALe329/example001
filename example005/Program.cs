bf1b9277572738997f6d32d49fd2692d38d499c2
ALe329 <ale329@ya.ru>
2022-06-07 10:00:42 +0300

    example 5:wq :wq

---
+++ example005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "lex") {
+    Console.Write("Приветствую Вас сударь  " + userName);
+}
+else {
+Console.Write("Привет " + userName);
+}
